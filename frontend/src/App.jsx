634ba8b05f161b4457c616c0073c0300a94cb0ce
Peter Dev <malakulkabirarrabbani@gmail.com>
2025-09-15 21:44:20 +0700

    update version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import ClavoxLoadingScreen from "./pages/LoadingScreen";
 import LoginScreen from "./pages/Auth/LoginScreen";
 import OTPVerificationScreen from "./pages/Auth/OtpVerification";
-
+import RegisterScreen from "./pages/Auth/RegisterScreen";
+import SuccessScreen from "./components/common/SuccessScreen"; // ✨ Impor SuccessScreen
+import ProcessingScreen from "./components/common/ProcessingScreen";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/loading" element={<ClavoxLoadingScreen />} />
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/verification" element={<OTPVerificationScreen/>} />
-      </Routes>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/loading" />
+        </Route>
+        <Route path="/loading" component={ClavoxLoadingScreen} />
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/verification" component={OTPVerificationScreen} />
+        <Route path="/register" component={RegisterScreen} />
+        <Route path="/success" component={SuccessScreen} /> {/* ✨ Tambahkan rute ini */}
+        <Route path="/processing" component={ProcessingScreen} />
+      </Switch>
     </Router>
   );
 }
